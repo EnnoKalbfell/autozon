66901c86bb4063aa3e07f36bbd78c40a495b28e3
Joel Oppliger <joel.oppliger@noseryoung.ch>
2021-04-03 15:18:10 +0200

    added product card component and page

--- Frontend/autozon/src/app/app-routing.module.ts
+++ Frontend/autozon/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductOverviewComponent } from './pages/product-overview/product-overview.component';
 
 const routes: Routes = [
+  { path: '', component: ProductOverviewComponent },
 ];
 
 @NgModule({
--- Frontend/autozon/src/app/pages/pages.module.ts
+++ Frontend/autozon/src/app/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
+import { ProductOverviewComponent } from './product-overview/product-overview.component';
 
 
 
 @NgModule({
   declarations: [
     // enter page declaration
+    ProductOverviewComponent
   ],
   imports: [
     CommonModule,
