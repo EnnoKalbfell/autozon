c1862bb63cc28d3549e324ba6401080395c2b49c
Enno Kalbfell <enno.kalbfell@noseryoung.ch>
2021-04-27 11:48:42 +0200

    Fix invalid information can not be sent

--- Backend/autozon/app/Http/Controllers/ProductController.php
+++ Backend/autozon/app/Http/Controllers/ProductController.php
@@ -9,65 +9,65 @@ use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-     /**
-     * Create a new controller instance.
-     * @return void
-     */
-    public function __construct() {
-      //
-    }
+  /**
+   * Create a new controller instance.
+   * @return void
+   */
+  public function __construct()
+  {
+    //
+  }
 
-    /**
-     * Show all products
-     *
-     * @return ['name','dealer','manufacturer','price','streetLegality','carId','shortDescription','category','serialNumber','preview','preview2','preview3']
-     */
-    public function getAllProducts()
-    {
-        return DB::table('product')->get();
-    }
+  /**
+   * Show all products
+   *
+   * @return ['name','dealer','manufacturer','price','streetLegality','carId','shortDescription','category','serialNumber','preview','preview2','preview3']
+   */
+  public function getAllProducts()
+  {
+    return DB::table('product')->get();
+  }
 
-    public static function productById($id)
-    {
-      return Product::where('id', $id)->first();
-    }
+  public static function productById($id)
+  {
+    return Product::where('id', $id)->first();
+  }
 
-    public function createProduct(Request $request)
-    {
-            // Server-side input validation
-            $this->validate($request, [
-              'name' => 'required|max:100',
-              'dealer'  => 'required',
-              'manufacturer'  => 'required:max150',
-              'price'  => 'required',
-              'streetLegality'  => 'required|boolean',
-              'carId' => 'required',
-              'shortDescription'  => 'required|max:200',
-              'category'  => 'required|max:255',
-              'serialNumber'  => 'required|max:100',
-              'preview' => 'required|max:150',
-              'preview2' => 'required|max:150',
-              'preview3' => 'required|max:150'
-            ]);
-      
-            try {
-              DB::table('product')->insert([   
-                'name' => $request->input('name'),
-                'dealer' => $request->input('dealer'),
-                'manufacturer' => $request->input('manufacturer'),
-                'price' => $request->input('price'),
-                'streetLegality' => $request->input('streetLegality'),
-                'carId' => $request->input('carId'),
-                'shortDescription' => $request->input('shortDescription'),
-                'category' => $request->input('category'),
-                'serialNumber' => $request->input('serialNumber'),
-                'preview' => $request->input('preview'),
-                'preview2' => $request->input('preview2'),
-                'preview3' => $request->input('preview3'),
-              ]);
+  public function createProduct(Request $request)
+  {
+    try {
+      // Server-side input validation
+      $this->validate($request, [
+        'name' => 'required|max:100',
+        'dealer'  => 'required',
+        'manufacturer'  => 'required:max150',
+        'price'  => 'required',
+        'streetLegality'  => 'required|boolean',
+        'carId' => 'required',
+        'shortDescription'  => 'required|max:200',
+        'category'  => 'required|max:255',
+        'serialNumber'  => 'required|max:100',
+        'preview' => 'required|max:150',
+        'preview2' => 'required|max:150',
+        'preview3' => 'required|max:150'
+      ]);
 
-            } catch (Exception $exception) {
-              return response()->json(['error' => 'Entry in database has failed!'], 400);
-            }
+      DB::table('product')->insert([
+        'name' => $request->input('name'),
+        'dealer' => $request->input('dealer'),
+        'manufacturer' => $request->input('manufacturer'),
+        'price' => $request->input('price'),
+        'streetLegality' => $request->input('streetLegality'),
+        'carId' => $request->input('carId'),
+        'shortDescription' => $request->input('shortDescription'),
+        'category' => $request->input('category'),
+        'serialNumber' => $request->input('serialNumber'),
+        'preview' => $request->input('preview'),
+        'preview2' => $request->input('preview2'),
+        'preview3' => $request->input('preview3'),
+      ]);
+    } catch (Exception $exception) {
+      return response()->json(['error' => 'Entry in database has failed!'], 400);
     }
+  }
 }
