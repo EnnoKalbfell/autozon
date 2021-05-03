2486575bee9e7ca5e91db9385a34648960f9e9cb
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2021-05-03 18:46:10 +0200

    add html and css

--- Frontend/autozon/src/app/app-routing.module.ts
+++ Frontend/autozon/src/app/app-routing.module.ts
@@ -2,16 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { ProductOverviewComponent } from './pages/product-overview/product-overview.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+
 
 const routes: Routes = [
-  { path: '',    
-    component: BaseLayoutComponent, 
+  {
+    path: '',
+    component: BaseLayoutComponent,
     children: [
-      { path: '',
+      {
+        path: '',
         component: ProductOverviewComponent
       }
     ]
-   },
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  }
 ];
 
 @NgModule({
--- Frontend/autozon/src/app/pages/pages.module.ts
+++ Frontend/autozon/src/app/pages/pages.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 import { ProductOverviewComponent } from './product-overview/product-overview.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
 
 
 
 @NgModule({
   declarations: [
     // enter page declaration
-    ProductOverviewComponent
+    ProductOverviewComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
