bb952646dc23b3cf244659713ffa28ab46374b5b
Joel Oppliger <joel.oppliger@noseryoung.ch>
2021-05-04 10:24:19 +0200

    added full user

---
+++ Frontend/autozon/src/app/core/models/user.model.ts
@@ -0,0 +1,16 @@
+import { NumberSymbol } from "@angular/common";
+
+export interface IUser{
+    id: number,
+    lastName: string,
+    firstName: string,
+    companyName: string,
+    email: string,
+    phone: number,
+    streetAndHouseNumber: string,
+    zipCode: string,
+    city: string,
+    country: string,
+    role: string,
+    verified: boolean
+}
