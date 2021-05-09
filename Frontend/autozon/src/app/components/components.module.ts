0ba623fdfa0087d0116a988f7679194b755a93a2
Stephanie Steiner <ssteiner@besonet.ch>
2021-05-09 11:24:39 +0200

    Add modal to confirm deletation

--- Frontend/autozon/src/app/components/components.module.ts
+++ Frontend/autozon/src/app/components/components.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ProductCardComponent } from './product-card/product-card.component';
-
+import { DeleteConfirmationDialogComponent } from './delete-confirmation-dialog/delete-confirmation-dialog.component';
 
 @NgModule({
   declarations: [
     // enter component declaration to use in page or wherever else
-    ProductCardComponent
+    ProductCardComponent,
+    DeleteConfirmationDialogComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    ProductCardComponent
+    ProductCardComponent,
+    DeleteConfirmationDialogComponent
   ]
 })
 export class ComponentsModule { }
--- Frontend/autozon/src/app/components/product-card/product-card.component.ts
+++ Frontend/autozon/src/app/components/product-card/product-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/core/models/product.model';
 import { ProductService } from 'src/app/core/services/product/product.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteConfirmationDialogComponent } from '../delete-confirmation-dialog/delete-confirmation-dialog.component';
 
 @Component({
   selector: 'app-product-card',
@@ -13,7 +15,7 @@ export class ProductCardComponent implements OnInit {
   showMore = false;
   route: string = window.location.pathname;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
@@ -24,13 +26,25 @@ export class ProductCardComponent implements OnInit {
     return false;
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DeleteConfirmationDialogComponent, {
+      width: 'auto',
+      data: this.product.name || ''
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.deleteProduct();
+      }
+    });
+  }
+
   deleteProduct(): void {
-    // TODO: Add dialog to confirm deleting before actually deleting product
     if (this.product.id) {
       this.productService.deleteMyProduct(this.product.id).subscribe((res: JSON) => {
         if (res !== undefined) {
           // Reload to not show deleted product anymore
-          window.location.reload();
+          window.location.reload(); // TODO: Maybe solve reloading nicer
         }
       });
     }
