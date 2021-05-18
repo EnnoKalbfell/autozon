306d12f61cdb8b548ec428a090c91aa0705be41a
Stephanie Steiner <ssteiner@besonet.ch>
2021-05-18 10:06:30 +0200

    Remove unit tests

--- Frontend/autozon/src/app/mocks/MatDialogMock.ts
+++
@@ -1,9 +0,0 @@
-import { of } from 'rxjs';
-
-export class MatDialogMock {
-  open(): any {
-   return {
-     afterClosed: () => of(true)
-   };
- }
-}
--- Frontend/autozon/src/app/mocks/routerMock.ts
+++
@@ -1,5 +0,0 @@
-export class RouterMock {
-  navigate(route: string): string {
-    return route;
-  }
-}
